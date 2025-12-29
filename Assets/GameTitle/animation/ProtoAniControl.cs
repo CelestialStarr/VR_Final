f83ab0d1a08ce53c3886cffa6c87347eaa55e112
Whitey-Bi <1357690460@qq.com>
2025-12-29 16:51:23 +0800

    开场动画

---
+++ Assets/GameTitle/animation/ProtoAniControl.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ProtoAniControl : MonoBehaviour
+{
+    public Animator anim;
+
+    public void SetStop()
+    {
+        anim.SetBool("Arrived", true);
+    }
+    public void LoadTownScene()
+    {
+        SceneManager.LoadScene("Town");
+    }
+}
